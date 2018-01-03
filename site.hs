8bf9b59a6ef5787c02621e526461070938cee792
Mike McGirr <mike@oatsmail.com>
2018-01-03 01:27:24 -0800

    Add a very basic robots.txt

--- site.hs
+++ site.hs
@@ -19,6 +19,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "robots.txt" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
